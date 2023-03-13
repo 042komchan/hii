447fe22cb7828031205d85790ad0765bf17b5350
042komchan <komchan.ying@mail.kmutt.ac.th>
2023-03-13 17:08:02 +0700

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main(string[] args){
+        
+        int day = int.Parse (Console.ReadLine());
+
+
+        string coffee = (Console.ReadLine());
+
+
+        int time = int.Parse (Console.ReadLine());
+
+
+
+
+
+        if (coffee == "Breakfast Set"){ 
+            if(day >=1 && day <= 7){
+                if (time >=8 && time <=18){
+                    {
+                        Console.WriteLine ("");
+                    }
+                }
+            } if((!(day >=1 && day <= 7))||(!(time >=8 && time <=18))){
+                 
+                        Console.WriteLine ("Please enter a valid menu");
+                    
+                
+            } 
+        }
+
+
+         if (coffee == "Weekend Set"){ 
+            if(day >=1 && day <= 7){
+                if (time >=8 && time <=18){
+                    {
+                        Console.WriteLine ("");
+                    }
+                }
+            } if((!(day >=1 && day <= 7))||(!(time >=8 && time <=18))){
+                 
+                        Console.WriteLine ("Please enter a valid menu");
+                    
+                
+            } 
+        }
+
+
+        if (coffee == "coffee"){ 
+            if(day >=1 && day <= 7){
+                if (time >=8 && time <=18){
+                    {
+                        Console.WriteLine ("");
+                    }
+                }
+            } if((!(day >=1 && day <= 7))||(!(time >=8 && time <=18))){
+                 
+                        Console.WriteLine ("Please enter a valid menu");
+                    
+                
+            } 
+        }
+
+    }
+}
